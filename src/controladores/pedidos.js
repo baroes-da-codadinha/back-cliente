f9f6222292a4e65aa7e8a734d8a9897031ad503d
iostsicaro <iostsicaro@gmail.com>
2021-08-22 23:15:01 -0300

    commit icaro mudanças no back

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -1,18 +1,15 @@
 const knex = require('../conexao');
 
-
 const cadastrarPedidos = async (req, res) => {
-const { restaurante_id, carrinho, endereco  } = req.body
-let subtotal = 0;
-//carrinho = [{id,  quantidade}, {id, quantidade},]
+    const { restaurante_id, carrinho, endereco } = req.body
+
+    let subtotal = 0;
+    //carrinho = [{id,  quantidade}, {id, quantidade},]
     try {
-        
         carrinho.forEach(produto => {
-            await knex('produtos').where({id: produto.id, ativo: true});
+            await knex('produtos').where({ id: produto.id, ativo: true });
 
         });
-
-        
     } catch (error) {
         return res.status(400).json(error.message);
     }
