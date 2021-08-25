2ffbea392ff7d4a955902678fdf9e926c35ef4bb
iostsicaro <iostsicaro@gmail.com>
2021-08-25 18:46:15 -0300

    commit icaro

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -1,15 +1,5 @@
 const knex = require('../conexao');
-
-// const listarPedido = async (req, res) => {
-//     const { consumidor } = req;
-
-//     try {
-        
-//     } catch (error) {
-//         return res.status(400).json(error.message);
-//     }
-// }
-
+ 
 const cadastrarPedidos = async (req, res) => {
     const { consumidor } = req;
         
@@ -20,12 +10,31 @@ const cadastrarPedidos = async (req, res) => {
             return res.status(404).json('Endereco não foi encontrado.');
         }
 
-        const cadastrarPedido = await knex('pedido').insert({ consumidor_id: consumidor.id, restaurante_id: restaurante.id, endereco_id: encontrarEndereco.id, subtotal, taxa, total });
+        const encontrarProdutos = await knex('carrinho').where({ consumidor_id: consumidor.id });
+
+        if (!encontrarProdutos) {
+            return res.status(404).json('Carrinho vazio.')
+        }
+
+        let subtotal = 0, total = 0;
+        encontrarProdutos.forEach(produto => {
+            subtotal = produto.valor_pagamento + subtotal;
+        });
+
+        const encontrarRestaurante = await knex('restaurantes').where({ id: encontrarProdutos[0].restaurante_id }).first();
+
+        if (!encontrarRestaurante) {
+            return res.status(404).json('Restaurante não foi encontrado.');
+        }
+
+        total = subtotal + encontrarRestaurante.taxa_entrega;
+
+        const cadastrarPedido = await knex('pedido').insert({ consumidor_id: consumidor.id, restaurante_id: encontrarRestaurante.id, endereco_id: encontrarEndereco.id, subtotal, taxa: encontrarRestaurante.taxa_entrega, total });
 
         if (!cadastrarPedido) {
             return res.status(404).json('Pedido não foi cadastrado.');
         }
-
+    
         return res.status(200).json('Pedido Confirmado! Agora é só aguardar o seu pedido');
     } catch (error) {
         return res.status(400).json(error.message);
